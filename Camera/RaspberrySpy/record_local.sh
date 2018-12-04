c56b89e957ef1150fbf72aeeef988dc5ad20d4e1
Arthur Ceccotti <gmtuca@gmail.com>
2018-12-04 23:50:57 +0000

    Logging

--- Camera/RaspberrySpy/record_local.sh
+++ Camera/RaspberrySpy/record_local.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 #1-hour snapshots
-SNAPSHOT_LENGTH_MINUTES=60
+SNAPSHOT_LENGTH_MINUTES=30
 
 #delete snapshots older than 1-day
 CLEANUP_OLDER_THAN_MINUTES=1440
 
+echo "Starting recording session at $(date '+%Y-%b-%d-%Hh-%Mm-%Ss')" >> /home/pi/recording.log
+
 while true
 do
-	raspivid -o "/home/pi/usb/$(date '+%Y-%b-%d-%Hh-%Mm-%Ss').h264" -t $(($SNAPSHOT_LENGTH_MINUTES * 60 * 1000)) -rot 90 -w 640 -h 480 -fps 25
-	test $? -gt 128 && break;
-	echo "Snapshot completed at $(date '+%Y-%b-%d-%Hh-%Mm-%Ss')"
-	find /home/pi/usb/. -maxdepth 1 -mmin +$(($CLEANUP_OLDER_THAN_MINUTES)) -exec rm {} \;
+        raspivid -o "/home/pi/usb/$(date '+%Y-%b-%d-%Hh-%Mm-%Ss').h264" -t $(($SNAPSHOT_LENGTH_MINUTES * 60 * 1000)) -rot$
+        test $? -gt 128 && break;
+        echo "Snapshot completed at $(date '+%Y-%b-%d-%Hh-%Mm-%Ss')" >> /home/pi/recording.log
+        find /home/pi/usb/. -maxdepth 1 -mmin +$(($CLEANUP_OLDER_THAN_MINUTES)) -exec rm {} \;
 done
