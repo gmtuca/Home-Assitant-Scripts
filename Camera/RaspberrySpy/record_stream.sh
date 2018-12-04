e3c675a2b5620407aa65636acee7e9238900b16c
Arthur Ceccotti <gmtuca@gmail.com>
2018-12-04 23:43:04 +0000

    Slight refactoring

--- Camera/RaspberrySpy/record_stream.sh
+++ Camera/RaspberrySpy/record_stream.sh
@@ -2,6 +2,4 @@
 PORT_NUMBER=8160
 
 #Streams are available within the network at VLC > Media > Open Network Stream > ip:$PORT_NUMBER
-
-killall raspivid
 raspivid -o - -t 0 -rot 90 -w 640 -h 480 -fps 25 | cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:'$PORT_NUMBER'}' :demux=h264
